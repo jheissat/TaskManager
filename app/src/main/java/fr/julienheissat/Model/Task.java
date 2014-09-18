dd6b22cc7c6e362280afd479fb37bb99c8a94320
juju <julien.heissat@gmail.com>
2014-09-18 19:00:37 +0200

    Initial changes based on discussion with Benoît.

---
+++ app/src/main/java/fr/julienheissat/Model/Task.java
@@ -0,0 +1,132 @@
+package fr.julienheissat.model;
+
+import android.location.Address;
+
+/**
+ * Created by juju on 15/09/2014.
+ */
+public class Task {
+
+    private String name;
+    private boolean complete;
+    private long id;
+    private String address;
+    private double latitude;
+    private double longitude;
+
+    public long getId()
+    {
+        return id;
+    }
+
+    public void setId(long id)
+    {
+        this.id = id;
+    }
+
+    public Task(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isComplete() {
+        return complete;
+
+    }
+
+    public void setComplete(boolean complete) {
+        this.complete = complete;
+    }
+
+    public void toggleComplete() {
+        complete  = !complete;
+
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+
+    public void setAddress (Address a)
+    {
+        if (null==a)
+        {
+            address = null;
+            latitude=0;
+            longitude=0;
+        }
+        else
+        {
+           int maxAddressLine = a.getMaxAddressLineIndex();
+           StringBuffer sb = new StringBuffer("");
+
+            for (int i=0;i<maxAddressLine;i++)
+            {
+            sb.append(a.getAddressLine(i)+" ");
+            }
+
+            address=sb.toString();
+            latitude=a.getLatitude();
+            longitude=a.getLongitude();
+
+        }
+
+
+    }
+
+    public boolean hasAddress() {
+        return (null != address);
+
+    }
+
+    public double getLatitude()
+    {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude)
+    {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude()
+    {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude)
+    {
+        this.longitude = longitude;
+    }
+
+    public void setTask(long id,String name,boolean complete,String address,double latitude, double longitude)
+    {
+        this.id=id;
+        this.name=name;
+        this.complete=complete;
+        this.address=address;
+        this.latitude=latitude;
+        this.longitude=longitude;
+
+    }
+
+
+}
---
+++ app/src/main/java/fr/julienheissat/Model/TaskList.java
@@ -0,0 +1,189 @@
+package fr.julienheissat.model;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+
+import static fr.julienheissat.database.TaskSQLiteOpenHelper.TASK_ADDRESS;
+import static fr.julienheissat.database.TaskSQLiteOpenHelper.TASK_COMPLETE;
+import static fr.julienheissat.database.TaskSQLiteOpenHelper.TASK_ID;
+import static fr.julienheissat.database.TaskSQLiteOpenHelper.TASK_LATITUDE;
+import static fr.julienheissat.database.TaskSQLiteOpenHelper.TASK_LONGITUDE;
+import static fr.julienheissat.database.TaskSQLiteOpenHelper.TASK_NAME;
+import static fr.julienheissat.database.TaskSQLiteOpenHelper.TASK_TABLE;
+
+/**
+ * Created by juju on 18/09/2014.
+ */
+public class TaskList
+{
+
+    private ArrayList<Task> taskList;
+    private SQLiteDatabase database;
+    private ArrayList<IModelListener> adapterListOfListener;
+
+    public TaskList(SQLiteDatabase database)
+    {
+        this.database = database;
+        loadTasks();
+        adapterListOfListener = new ArrayList<IModelListener>();
+    }
+
+    private void loadTasks()
+    {
+        taskList = new ArrayList<Task>();
+        Cursor tasksCursor  = database.query(
+                TASK_TABLE,
+                new String[] {TASK_ID, TASK_NAME,TASK_COMPLETE,TASK_ADDRESS,TASK_LATITUDE,TASK_LONGITUDE},
+                null, null, null, null,String.format("%s,%s",TASK_COMPLETE,TASK_NAME));
+        tasksCursor.moveToFirst();
+        Task t;
+
+        if (!tasksCursor.isAfterLast())
+        {
+            do
+            {
+                long id = tasksCursor.getLong(0);
+                String name= tasksCursor.getString(1);
+                String boolValue=tasksCursor.getString(2);
+                boolean complete=Boolean.parseBoolean(boolValue);
+                String address = tasksCursor.getString(3);
+                float latitude = tasksCursor.getFloat(4);
+                float longitude = tasksCursor.getFloat(5);
+
+                t=new Task(name);
+                t.setTask(id,name,complete,address,latitude,longitude);
+                taskList.add(t);
+
+            }
+            while (tasksCursor.moveToNext());
+        }
+
+        tasksCursor.close();
+
+    }
+
+    public void addTask(Task t)
+    {
+        assert (null != t);
+
+        ContentValues values = new ContentValues();
+        values.put(TASK_NAME,t.getName());
+        values.put(TASK_COMPLETE,Boolean.toString(t.isComplete()));
+        values.put(TASK_ADDRESS,t.getAddress());
+        values.put(TASK_LATITUDE,t.getLatitude());
+        values.put(TASK_LONGITUDE,t.getLongitude());
+
+        long id = database.insert(TASK_TABLE, null, values);
+        t.setId(id);
+
+        taskList.add(t);
+
+    }
+
+    private void saveTask(Task t)
+
+    {
+        assert (null != t);
+
+        ContentValues values = new ContentValues();
+        values.put(TASK_NAME,t.getName());
+        values.put(TASK_COMPLETE,Boolean.toString(t.isComplete()));
+        values.put(TASK_ADDRESS,t.getAddress());
+        values.put(TASK_LATITUDE,t.getLatitude());
+        values.put(TASK_LONGITUDE,t.getLongitude());
+
+        long id = t.getId();
+        String where = String.format("%s = ?",TASK_ID);
+        database.update(TASK_TABLE,values,where,new String[]{id+""});
+    }
+
+
+    public void deleteTasks(Long[]ids)
+    {
+        StringBuffer idList = new StringBuffer();
+        for (int i=0;i<ids.length;i++)
+        {
+            idList.append(ids[i]);
+            if(i<ids.length-1)
+            {
+                idList.append(",");
+            }
+
+        }
+
+        String where = String.format("%s in (%s)",TASK_ID,idList);
+        database.delete(TASK_TABLE,where,null);
+    }
+
+    public int getSize()
+    {
+        return taskList.size();
+    }
+
+    public Task get(int i)
+    {
+        return taskList.get(i);
+    }
+
+    public void toggleTask(int position)
+    {
+        Task task = get(position);
+        task.toggleComplete();
+        saveTask(task);
+        updateAll();
+    }
+
+    public Long[] removeCompletedTasks() {
+        ArrayList<Long> completedIds= new ArrayList<Long>();
+        ArrayList<Task> completedTasks= new ArrayList<Task>();
+        for (Task t:taskList)
+        {
+            if (t.isComplete())
+            {
+                completedIds.add(t.getId());
+                completedTasks.add(t);
+            }
+        }
+
+        taskList.removeAll(completedTasks);
+        updateAll();
+        return completedIds.toArray(new Long[]{});
+    }
+
+
+    //Interface to update adapter listeners when some specific changes to taskList happens
+
+    public static interface IModelListener
+    {
+        public void modelChanged(TaskList taskList);
+    }
+
+    public void updateAll()
+    {
+        for (IModelListener listener:adapterListOfListener)
+        {
+            listener.modelChanged(this);
+        }
+    }
+
+    public void register(IModelListener listener)
+
+    {
+        adapterListOfListener.add(listener);
+    }
+
+
+
+    public void unregister(IModelListener listener)
+    {
+        adapterListOfListener.remove(listener);
+    }
+    public void unRegisterAll()
+    {
+        adapterListOfListener.removeAll(adapterListOfListener);
+    }
+
+}
